91e403fc90759b8cd62d403aa9d23989cd29b165
Aaron Knol <aaron.knol@cultureamp.com>
2020-03-10 23:06:52 +1100

    EditWine converted to function component and hooks

--- src/components/EditWine.js
+++ src/components/EditWine.js
@@ -2,39 +2,33 @@ import React, { Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
 import StarRating from './StarRating';
 import formatToCents from '../utilities';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-class EditWine extends React.Component {
-    nameInput = React.createRef();
-    typeInput = React.createRef();
-    priceInput = React.createRef();
-    
+function EditWine (props) {
+    const [selectedWine, setSelectedWine] = useState({
+        name: '',
+        price: '',
+        type: '',
+        rating: ''
+    });
+    const [currentRating, setCurrentRating] = useState('');
+    const [nameValue, setNameValue] = useState('Pepp');
 
-    constructor(props) {
-        super();
-        
-        this.state = {
-            selectedWine : {
-                name: '',
-                price: '',
-                type: '',
-                rating: ''
-            },
-            currentRating: ''
-        }
-    }
+    // const nameInput = React.createRef();
+    // const typeInput = React.createRef();
+    // const priceInput = React.createRef();
 
-    componentDidMount() {
-        Object.keys(this.props.wines).map( key => {
-            if (key === this.props.match.params.wineId) {
-                this.setState({
-                    selectedWine: this.props.wines[key],
-                    currentRating: this.props.wines[key].rating
-                });
+    useEffect(() => {
+        Object.keys(props.wines).map( key => {
+            if (key === props.match.params.wineId) {
+                setSelectedWine(props.wines[key]);
+                setCurrentRating(props.wines[key].rating);
             }
         });
-    }
+    }, []);
     
-    submitHandler = (e) => {
+    const submitHandler = (e) => {
         e.preventDefault();
         
         const nameValue = this.nameInput.current.value;
@@ -45,77 +39,98 @@ class EditWine extends React.Component {
             name: nameValue,
             type: typeValue,
             price: formatToCents(parseFloat(priceValue)),
-            rating: this.state.currentRating
+            rating: currentRating
         };
 
-        this.props.updateWine(this.props.match.params.wineId, newWine);
-        this.props.history.push('/');
+        props.updateWine(this.props.match.params.wineId, newWine);
+        props.history.push('/');
     }
 
-    ratingSelected = (selected) => {
-        this.setState({
-            currentRating: selected
-        });
+    const ratingSelected = (selected) => {
+        setCurrentRating(selected);
     }
 
-    render() {
-        return (
-            <form onSubmit={this.submitHandler}>
-                <fieldset>
-                    <h1>Edit wine</h1>
+    const onChangeHandler = (e) => {
+        console.log('gotta change something! ', e.target.name, ' ', e.target.value);
+        
+        if (e.target.name === 'name') {console.log('here')
+            setSelectedWine(({
+                ...selectedWine,
+                name: e.target.value
+            }));
+        } else if (e.target.name === 'type') {
+            setSelectedWine({
+                ...selectedWine,
+                type: e.target.value
+            });
+        } else if (e.target.name === 'price') {
+            setSelectedWine({
+                ...selectedWine,
+                price: e.target.value
+            });
+        }
+    }
 
-                    <div className="form-group">
-                        <label htmlFor="name">Name</label>
+
+    return (
+        <form onSubmit={submitHandler}>
+            <fieldset>
+                <h1>Edit wine</h1>
+
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input 
+                        // ref={nameInput}
+                        type="text" 
+                        id="name"
+                        name="name"
+                        value={selectedWine.name}
+                        onChange={onChangeHandler} />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="type">Type</label>
+                    <select 
+                        id="type"
+                        // ref={typeInput}
+                        name="type"
+                        value={selectedWine.type}
+                        onChange={onChangeHandler}>
+                        <option value="red">Red</option>
+                        <option value="white">white</option>
+                        <option value="rose">rose</option>
+                    </select>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="price">Price</label>
+                    <div className="input-wrapper">
+                        <span className="currency">$</span>
                         <input 
-                            ref={this.nameInput}
+                            // ref={priceInput}
                             type="text" 
-                            id="name"
-                            value={this.state.selectedWine.name}
-                            onChange={this.onChangeHandler} />
+                            id="price"
+                            name="price"
+                            value={selectedWine.price}
+                            onChange={onChangeHandler}/>
                     </div>
+                </div>
 
-                    <div className="form-group">
-                        <label htmlFor="type">Type</label>
-                        <select 
-                            id="type"
-                            ref={this.typeInput}
-                            value={this.state.selectedWine.type}
-                            onChange={this.onChangeHandler}>
-                            <option value="red">Red</option>
-                            <option value="white">white</option>
-                            <option value="rose">rose</option>
-                        </select>
-                    </div>
+                <div className="form-group">
+                    <h2>Rating</h2>
+                    
+                    <StarRating
+                        numberOfStars="5"
+                        onClick={ratingSelected}
+                        value={currentRating}
+                        onChange={onChangeHandler}>
+                    </StarRating>
+                </div>
 
-                    <div className="form-group">
-                        <label htmlFor="price">Price</label>
-                        <div className="input-wrapper">
-                            <span className="currency">$</span>
-                            <input 
-                                ref={this.priceInput}
-                                type="text" 
-                                id="price"
-                                value={this.state.selectedWine.price}
-                                onChange={this.onChangeHandler}/>
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <h2>Rating</h2>
-                        
-                        <StarRating
-                            numberOfStars="5"
-                            onClick={this.ratingSelected}
-                            value={this.state.currentRating}
-                            onChange={this.onChangeHandler}>
-                        </StarRating>
-                    </div>
-
-                    <button type="submit">Save changes</button>
-                </fieldset>
-            </form>
-        );
-    }
+                <button type="submit">Save changes</button>
+            </fieldset>
+        </form>
+    );
 }
 
 export default withRouter(EditWine);
