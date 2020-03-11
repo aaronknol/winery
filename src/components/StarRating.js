a818fb09fec51098e7d2c982cd945f71a217b4b2
Aaron Knol <aaron.knol@cultureamp.com>
2020-03-11 14:53:38 +1100

    StarRating changed to functional component

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,37 +1,30 @@
 import React, { Fragment } from 'react';
 
-class StarRating extends React.Component {
+function StarRating (props) {
+    // create an array that is length of numberOfStars, with values 0 to numberOfStars
+    const numberOfStarsArray = Array.from(Array(parseInt(props.numberOfStars, 10)).keys());
 
-    constructor() {
-        super();
-    }
-
-    clickHandler = (e) => {
+    const clickHandler = (e) => {
         const selectedValue = e.target.value;
-
-        this.props.onClick(selectedValue);
+        props.onClick(selectedValue);
     }
 
-    render() {
-        // create an array that is length of numberOfStars, with values 0 to numberOfStars
-        const numberOfStarsArray = Array.from(Array(parseInt(this.props.numberOfStars, 10)).keys());
+    return (
 
-        return (
-                Object.keys(numberOfStarsArray).map((key) => (
-                    <Fragment key={key}>
-                        <label htmlFor={`rating-${parseInt(key, 10) + 1}`}>{ parseInt(key, 10) + 1 }</label>
-                        <input 
-                            type="radio" 
-                            name="rating" 
-                            id={`rating-${parseInt(key, 10) + 1}`} 
-                            value={parseInt(key, 10) + 1} 
-                            onChange={this.clickHandler}
-                            checked={parseInt(this.props.value, 10) === parseInt(key, 10) + 1}
-                        />
-                    </Fragment>
-                ))
-        )
-    }
-};
+        Object.keys(numberOfStarsArray).map((key) => (
+            <Fragment key={key}>
+                <label htmlFor={`rating-${parseInt(key, 10) + 1}`}>{ parseInt(key, 10) + 1 }</label>
+                <input 
+                    type="radio" 
+                    name="rating" 
+                    id={`rating-${parseInt(key, 10) + 1}`} 
+                    value={parseInt(key, 10) + 1} 
+                    onChange={clickHandler}
+                    checked={parseInt(props.value, 10) === parseInt(key, 10) + 1}
+                />
+            </Fragment>
+        ))
+    )
+}
 
 export default StarRating;
