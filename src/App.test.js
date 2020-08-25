71af4b646a0ae69b1366e1ad8c87a7f3770b6500
Aaron Knol <aaron.knol@cultureamp.com>
2020-08-25 20:12:30 +1000

    Updated App.test to included <BrowserRouter> around <App>

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
@@ -11,5 +12,5 @@ import App from './App';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
 })
